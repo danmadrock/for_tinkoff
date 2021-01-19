df80685aae92358c04097e059340b8ec41ad374b
danmadrock <70092308+danmadrock@users.noreply.github.com>
2021-01-19 09:58:23 +0300

    update

--- invest.py
+++ invest.py
@@ -1,7 +1,5 @@
 """
-for better understanding download ipynb file:
-
-or github link:
+!!! for better understanding download ipynb notebook:
 
 """
 
@@ -15,7 +13,7 @@ plt.style.use('ggplot')
 plt.rcParams['figure.figsize'] = (10, 5)
 
 
-data = pd.read_csv('YNDX_160101_161231.csv')
+data = pd.read_csv('YNDX_160101_161231.csv') #download at my github: https://github.com/danmadrock/for_tinkoff
 data.rename(columns={data.columns[0]: 'Date',
                      data.columns[1]: 'Time',
                      data.columns[2]: 'Open',
@@ -38,50 +36,82 @@ for i in range(len(data['Close'])):
 
 data['Difference'] = new_col
 
+print("""Существует три алгоритма для поиска акций:
+0 - Алгоритм поиска акций для одной транзакции
+1 - Алгоритм поиска акций для двух транзакций
+2 - Алгоритм поиска акций для k транзакций
+""")
+answer = int(input('Каким аллгоритмом желаете воспользоваться: 0/1/2: '))
 
-def only_transaction(data):
+
+def only_transaction(data, answer):
   stocks = 0
   profit = 0
   capital = int(input("Введите начальный капитал: "))
   
   price_to_buy = data['Low'].min()
   price_to_sell = data['High'].max()
-
-  # проверка того, что high.max произошел позже low.min
-  #if
+  low_price_index = data[data['Low'] == data['Low'].min()].index[0] 
+  high_price_index = data[data['High'] == data['High'].max()].index[0] 
   
-  stocks = capital / price_to_buy
-  capital_prev = capital
-  capital_new = stocks * price_to_sell
-  stocks = 0
-  profit = capital_new - capital_prev
+  # проверка того, что high.max произошел позже low.min
+  if low_price_index < high_price_index:
+    stocks = capital / price_to_buy
+    capital_prev = capital
+    capital_new = stocks * price_to_sell
+    stocks = 0
+    profit = capital_new - capital_prev
+    date_buy = data[data['Low']==data['Low'].min()]['Date']
+    date_sell = data[data['High']==data['High'].max()]['Date']
 
-  date_buy = data[data['Low']==data['Low'].min()]['Date']
-  date_sell = data[data['High']==data['High'].max()]['Date']
+    start_point = int(date_buy.index[0])
+    finish_point = int(date_sell.index[0])
 
-  start_point = int(date_buy.index[0])
-  finish_point = int(date_sell.index[0])
+    date_buy.index = [0]
+    date_sell.index = [0]
+    days = date_sell - date_buy
 
-  date_buy.index = [0]
-  date_sell.index = [0]
-  days = date_sell - date_buy
-
-  difference = []
-
-
-  for i in range(finish_point-start_point):
-    i += 4187
-    result = data['Difference'][i]
-    difference.append(result)
+    difference = []
 
 
-  print(f"""Результат транзакции:
-  Прибыль: {profit} \n
-  Дата покупки: {date_buy} \n
-  Дата продажи: {date_sell} \n
-  Изменения стоимости акций: {difference} \n
-  """)
+    for i in range(finish_point-start_point):
+      i += 4187
+      result = data['Difference'][i]
+      difference.append(result)
+      
+      
+    print(f"""Результат транзакции:
+    Прибыль: {profit} \n
+    Дата покупки: {date_buy} \n
+    Дата продажи: {date_sell} \n
+    Изменения стоимости акций: {difference} \n
+    """)
+  else:
+    print('Невозможно выполнить операцию. Дата продажи раньше даты покупки!')
+
         
         
-def two_transactions(data):
-  pass
+def two_transactions(data, answer):
+  if answer != 1:
+    pass
+  else:
+    profit = 0
+    days_list = []
+    capital = int(input('Введите капитал: '))
+    transactions = 2
+    difference = []
+    days_list = []
+    
+    
+    for i in range(transactions):
+      date_buy = data[data['Low']==data['Low'].min()]['Date']
+      date_sell = data[data['High']==data['High'].max()]['Date']
+      # сделать проверку на дату транзакции
+      
+      start_point = date_buy.index[0]
+      finish_point = date_sell.index[0]
+      date_buy.index = [0]
+      date_sell.index = [0]
+      days = date_sell - date_buy
+      days_list.append(days)
+    
